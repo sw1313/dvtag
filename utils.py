96b1f38448fb3b49484263fe4b3772390e3e24a7
sw1313 <hfzy2014@gmail.com>
2024-12-10 03:44:30 +0800

    Update utils.py
    由于avi对非ascii 字符tag的支持也特别差，添加avi转mp4功能

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ If users need more flexible encoding options or advanced features(like parallel 
 __all__ = [
     "wav_to_flac",
     "wav_to_mp3",
+    "avi_to_mp4",
 ]
 
 import logging
@@ -14,11 +15,10 @@ import subprocess
 from pathlib import Path
 from typing import List
 
-
 def transcode_wav(dir: Path, format: str, options: List[str] = []):
     for dirpath, _, filenames in os.walk(dir):
         for filename_wav in filenames:
-            if not filename_wav.endswith(".wav"):
+            if not filename_wav.lower().endswith(".wav"):
                 continue
             filename_trans = filename_wav[:-3] + format
 
@@ -46,3 +46,35 @@ def wav_to_flac(dir: Path):
 
 def wav_to_mp3(dir: Path):
     transcode_wav(dir, "mp3", ["-b:a", "320k"])
+
+
+def transcode_avi(dir: Path, format: str, options: List[str] = []):
+    # 类似 transcode_wav 的逻辑，但是过滤 .avi 文件并转为 mp4
+    for dirpath, _, filenames in os.walk(dir):
+        for filename_avi in filenames:
+            if not filename_avi.lower().endswith(".avi"):
+                continue
+            filename_trans = filename_avi[:-3] + format
+
+            file_avi = os.path.join(dirpath, filename_avi)
+            file_trans = os.path.join(dirpath, filename_trans)
+            if os.path.exists(file_trans):
+                logging.warning(f"{filename_trans} already exists.")
+                continue
+
+            logging.info(f"Start transcoding {filename_avi} to {format}")
+
+            returncode = subprocess.call(
+                ["ffmpeg", "-i", file_avi, *options, file_trans], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT
+            )
+            if returncode == 0:
+                logging.info(f"Transcoded {filename_avi} successfully, deleting this source file")
+                os.remove(file_avi)
+            else:
+                logging.fatal(f"Failed to transcode {filename_avi} to {format}. Check your ffmpeg")
+
+
+def avi_to_mp4(dir: Path):
+    # 这里的转码参数可根据需要调整
+    # 使用拷贝流，如果报错则改用转码，如 ["-c:v", "libx264", "-c:a", "aac", "-strict", "experimental"]
+    transcode_avi(dir, "mp4", ["-c:v", "copy", "-c:a", "copy"])
